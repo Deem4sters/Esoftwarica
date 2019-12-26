76a7814ac066625abc9680ac1a9184e6bd78150e
Deem4sters <deem4sters@gmail.com>
2019-12-26 09:48:51 +0545

    Initial commit

---
+++ app/src/main/java/com/dee/esoftwarica/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.dee.esoftwarica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+ public EditText etUsername, etPassword;
+ public Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        
+
+
+    }
+
+}
