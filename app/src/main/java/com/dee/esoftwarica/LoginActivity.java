1efaf30ee91baede68719bb32d4bc1d0601dd906
Deem4sters <deem4sters@gmail.com>
2019-12-26 15:28:01 +0545

    Button Navigation

--- app/src/main/java/com/dee/esoftwarica/LoginActivity.java
+++ app/src/main/java/com/dee/esoftwarica/LoginActivity.java
@@ -2,9 +2,12 @@ package com.dee.esoftwarica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
  public EditText etUsername, etPassword;
@@ -19,6 +22,22 @@ public class LoginActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btnLogin);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etUsername.getText().toString().equals("softwarica") || etUsername.getText().toString().equals("Softwarica") && etPassword.getText().toString().equals("coventry")) {
+                    Toast.makeText(LoginActivity.this, "Username is correct", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Username or password Invalid", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
 
 
 
--- app/src/main/java/com/dee/esoftwarica/MainActivity.java
+++ app/src/main/java/com/dee/esoftwarica/MainActivity.java
@@ -1,8 +1,16 @@
 package com.dee.esoftwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.dee.esoftwarica.fragment.AboutFragment;
+import com.dee.esoftwarica.fragment.AddFragment;
+import com.dee.esoftwarica.fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottomNavigation);
+        navigationView.setOnNavigationItemSelectedListener(navListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()) {
+                case R.id.iteamHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.iteamStudent:
+                    selectedFragment = new AddFragment();
+                    break;
+                case R.id.iteamAbout:
+                    selectedFragment = new AboutFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameId, selectedFragment).commit();
+            return true;
+
+        }
+    };
 }
